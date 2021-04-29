85c3baba7d9fb1b8bc7f918b1e3c2605d27bcb5c
vcheckzen <18008498+vcheckzen@users.noreply.github.com>
2021-04-29 18:27:50 +0800

    refactor

---
+++ refresh.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+prefix='https://purge.jsdelivr.net/npm/logicdn/logi.im/api'
+apis=(
+    "$prefix/sentence/data.json"
+    "$prefix/links/data.json"
+)
+
+refresh() {
+    for i in {1..6}; do
+        curl "$1"
+        sleep 30
+    done
+}
+
+for api in "${apis[@]}"; do
+    refresh "$api" &
+done
+
+sleep 180
