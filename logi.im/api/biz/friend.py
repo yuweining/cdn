8c3b6fa77d16dd3acbd10ff3e9a4f2d881dc9a27
vcheckzen <18008498+vcheckzen@users.noreply.github.com>
2021-05-31 16:35:46 +0800

    alter online checking logic

--- logi.im/api/biz/friend.py
+++ logi.im/api/biz/friend.py
@@ -11,8 +11,8 @@ from concurrent.futures import ThreadPoolExecutor
 import requests
 from PIL import Image
 
-TIME_OUT = 15
-MAX_TRY = 4
+TIME_OUT = 20
+MAX_TRY = 3
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
 WHITE_LIST = ['dianr.cn']
 
